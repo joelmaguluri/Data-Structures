1df6ab77b24677f56d1ac0b4e08a6a37d9130210
sudeep987 <msudeep.joel@gmail.com>
2020-03-27 15:10:18 +0530

    modified construct Binary Tree.c

--- Trees/constructBinaryTree.c
+++ Trees/constructBinaryTree.c
@@ -4,36 +4,31 @@
 
 struct TreeNode *constructBinaryTree(int num)
 {
-  int arr[num], i;
-  for (i = 0; i < num; i++)
-  {
-    arr[i] = i + 1;
-  }
-  i = 0;
+  int value = 1;
   Queue *queue = (Queue *)malloc(sizeof(Queue)); //creating a queue
   queue->size = 0;
   queue->top = NULL;
   struct TreeNode *root = (struct TreeNode *)malloc(sizeof(struct TreeNode));
-  root->data = arr[i];
+  root->data = value;
   root->left = NULL;
   root->right = NULL;
   enqueue(queue, root);
   while (queue->size) //while stack is not zero
   {
     struct TreeNode *curr_node = (dequeue(queue))->node;
-    if (++i < num)
+    if (++value <= num)
     {
       struct TreeNode *left = (struct TreeNode *)malloc(sizeof(struct TreeNode));
-      left->data = arr[i];
+      left->data = value;
       left->left = NULL;
       left->right = NULL;
       curr_node->left = left;
       enqueue(queue, left);
     }
-    if (++i < num)
+    if (++value <= num)
     {
       struct TreeNode *right = (struct TreeNode *)malloc(sizeof(struct TreeNode));
-      right->data = arr[i];
+      right->data = value;
       right->left = NULL;
       right->right = NULL;
       curr_node->right = right;
