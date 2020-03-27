715a5362595870e18e7e536ddfff65ea91f79f47
sudeep987 <msudeep.joel@gmail.com>
2020-03-27 14:16:50 +0530

    Sorted the logic for Height of the tree

--- Trees/height.c
+++ Trees/height.c
@@ -1,21 +1,26 @@
 // I/P:                  1
 //                   2       3
-//                4    5   6   7
-//                               9
-// O/P: 3
+//                4    5
+//
+// O/P: 2
 
 #include <stdio.h>
 #include <stdlib.h>
 #include "treeADT.h"
 
-int height(struct TreeNode*root,int currrentheight)
+int height(struct TreeNode *root, int curentheight)
 {
-    if(root)
-        return currrentheight;
-    int left_height=height(root->left,currrentheight+1);
-    int right_height=height(root->right,currrentheight+1);
-    if(left_height>right_height)
-     return left_height;
-    else 
-     return right_height;
+    if (!root)
+        return curentheight - 1;
+    int left_height = height(root->left, curentheight + 1);
+    int right_height = height(root->right, curentheight + 1);
+    if (left_height > right_height)
+        return left_height;
+    else
+        return right_height;
+}
+void main()
+{
+    struct TreeNode *root = getSampleTree();
+    printf("height of the tree is %d", height(root, 0)); // height of the root node is 0
 }
